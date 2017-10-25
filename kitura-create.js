b08dd5bca9502f6c9fc2798a8e1680ac99c37724
Ian Partridge <i.partridge@uk.ibm.com>
2017-10-25 14:58:47 +0100

    Remove try/catch blocks

--- kitura-create.js
+++ kitura-create.js
@@ -6,14 +6,10 @@ const spawn = require('child_process').spawn;
 program
     .parse(process.argv);
 
-try {
-    let child = spawn('yo', ['swiftserver'], { stdio: 'inherit' });
-    child.on('error', (err) => {
-        console.error(err);
-    });
-    child.on('close', (code) => {
-        process.exit(code);
-    });
-} catch(err) {
+let child = spawn('yo', ['swiftserver'], { stdio: 'inherit' });
+child.on('error', (err) => {
     console.error(err);
-}
+});
+child.on('close', (code) => {
+    process.exit(code);
+});
--- kitura-idt.js
+++ kitura-idt.js
@@ -9,27 +9,23 @@ program
 
 const url = 'https://ibm.biz/kitura-idt';
 
-try {
-    // Run bash, setting stdin as a pipe
-    let child = spawn('bash', [], { stdio: ['pipe', 'inherit', 'inherit'] });
-    child.on('error', (err) => {
-        console.error(err);
-    });
-    child.on('close', (code) => {
-        process.exit(code);
-    });
-
-    // Ensure that Ctrl-C also kills the child
-    process.on('SIGINT', () => {
-        child.kill('SIGINT');
-        process.exit();
-    });
-
-    // Download the IDT installer and pipe it to the child
-    request
-        .get({url, followAllRedirects: true})
-        .on('error', (err) => { console.error(err); })
-        .pipe(child.stdin);
-} catch(err) {
+// Run bash, setting stdin as a pipe
+let child = spawn('bash', [], { stdio: ['pipe', 'inherit', 'inherit'] });
+child.on('error', (err) => {
     console.error(err);
-}
+});
+child.on('close', (code) => {
+    process.exit(code);
+});
+
+// Ensure that Ctrl-C also kills the child
+process.on('SIGINT', () => {
+    child.kill('SIGINT');
+    process.exit();
+});
+
+// Download the IDT installer and pipe it to the child
+request
+    .get({url, followAllRedirects: true})
+    .on('error', (err) => { console.error(err); })
+    .pipe(child.stdin);
