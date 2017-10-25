797911954568dfbb7b37caa62c56dee9c85ac780
Ian Partridge <i.partridge@uk.ibm.com>
2017-10-25 10:08:08 +0100

    Prefer let to var, and group require()s together

--- kitura-create.js
+++ kitura-create.js
@@ -1,14 +1,13 @@
 #!/usr/bin/env node
 
 const program = require('commander');
+const spawn = require('child_process').spawn;
 
 program
     .parse(process.argv);
 
-const spawn = require('child_process').spawn;
-
 try {
-    var child = spawn('yo', ['swiftserver'], { stdio: 'inherit' });
+    let child = spawn('yo', ['swiftserver'], { stdio: 'inherit' });
     child.on('error', (err) => {
         console.error(err);
     });
--- kitura-init.js
+++ kitura-init.js
@@ -1,14 +1,13 @@
 #!/usr/bin/env node
 
 const program = require('commander');
+const spawn = require('child_process').spawn;
 
 program
     .parse(process.argv);
 
-const spawn = require('child_process').spawn;
-
 try {
-    var child = spawn('yo', ['swiftserver', '--init'], { stdio: 'inherit' });
+    let child = spawn('yo', ['swiftserver', '--init'], { stdio: 'inherit' });
     child.on('error', (err) => {
         console.error(err);
     });
