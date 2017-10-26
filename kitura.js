c764754eba2533d2103104c7922db8dcd1da87f9
Ian Partridge <i.partridge@uk.ibm.com>
2017-10-26 13:43:13 +0100

    Remove init and create command aliases

--- kitura.js
+++ kitura.js
@@ -7,9 +7,9 @@ program
     .version(pjson.version)
     .description('Kitura command-line interface')
     .command('build', 'build the project in a local container')
-    .command('create', 'interactively create a Kitura project').alias('c')
+    .command('create', 'interactively create a Kitura project')
     .command('idt', 'install IBM Cloud Developer Tools')
-    .command('init', 'scaffold a bare-bones Kitura project').alias('i')
+    .command('init', 'scaffold a bare-bones Kitura project')
     .command('kit', 'download the KituraKit zip file')
     .command('run', 'run the project in a local container')
     .parse(process.argv);
