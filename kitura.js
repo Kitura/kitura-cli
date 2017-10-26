9e0395a8bfed3d0414c47aaba5d6192604dffdf0
Ian Partridge <i.partridge@uk.ibm.com>
2017-10-26 12:05:45 +0100

    Add `kitura build` command

--- kitura.js
+++ kitura.js
@@ -6,7 +6,8 @@ const program = require('commander');
 program
     .version(pjson.version)
     .description('Kitura command-line interface')
-    .command('init', 'scaffold a bare-bones Kitura project').alias('i')
+    .command('build', 'build the project in a local container')
     .command('create', 'interactively create a Kitura project').alias('c')
     .command('idt', 'install IBM Cloud Developer Tools')
+    .command('init', 'scaffold a bare-bones Kitura project').alias('i')
     .parse(process.argv);
