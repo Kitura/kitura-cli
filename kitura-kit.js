7ff3e41dae680e9fd2aa7b1f447b52a788856890
seabaylea <baileyc@uk.ibm.com>
2017-10-29 00:37:45 +0100

    Update 'kit' option to downloaded latest release zip file

--- kitura-kit.js
+++ kitura-kit.js
@@ -7,18 +7,34 @@ const request = require('request');
 program
     .parse(process.argv);
 
-const url = 'https://api.github.com/repos/IBM-Swift/KituraKit/zipball';
+const options = {
+    method: "GET",
+    followAllRedirects: true,
+    headers: {
+        "user-agent": "node.js"
+    }
+};
+
+const url = 'https://api.github.com/repos/IBM-Swift/KituraKit/releases';
 const filename = 'KituraKit.zip';
 
 // Download KituraKit from GitHub
 console.log('Downloading...');
 
-request
-    .get({url, followAllRedirects: true, headers: { 'User-Agent': 'node.js'}})
-    .on('error', (err) => {
-        console.error(chalk.red('Error: ') + 'download failed.');
-    })
-    .on('response', (response) => {
-        console.log('KituraKit downloaded to ' + filename);
-    })
-    .pipe(fs.createWriteStream(filename));
+request(url, options, function(error, response, body) {
+    if (error) {
+        console.error(chalk.red('Error: ') + ' failed to get releases from GitHub.');
+        return
+    }
+    var releases = JSON.parse(body);
+    var latestKit = releases[0].assets[0].browser_download_url
+    request
+        .get(latestKit, options)
+        .on('error', (err) => {
+            console.error(chalk.red('Error: ') + 'download failed.');
+        })
+        .on('response', (response) => {
+            console.log('KituraKit downloaded to ' + filename);
+        })
+        .pipe(fs.createWriteStream(filename));
+    });
