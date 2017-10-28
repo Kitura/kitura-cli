5175da56b63b790085c30c7ed23df5fdb45c9da9
Ian Partridge <i.partridge@uk.ibm.com>
2017-10-28 21:12:58 +0100

    init+create: run NPM install before invoking generator

--- kitura-create.js
+++ kitura-create.js
@@ -1,11 +1,20 @@
 #!/usr/bin/env node
 
+const chalk = require('chalk');
 const program = require('commander');
 const spawn = require('child_process').spawn;
+const spawnSync = require('child_process').spawnSync;
 
 program
     .arguments('[modelname]')
     .parse(process.argv);
+
+// Run NPM to install the Yeoman generator
+var npm = spawnSync('npm', ['install', '-g', 'generator-swiftserver'], { stdio: 'inherit' });
+if (npm.error) {
+    console.error(chalk.red('Error: ') + 'NPM package installation failed');
+    process.exit(npm.status);
+}
 
 let options = ['swiftserver']
 
--- kitura-init.js
+++ kitura-init.js
@@ -1,10 +1,19 @@
 #!/usr/bin/env node
 
+const chalk = require('chalk');
 const program = require('commander');
 const spawn = require('child_process').spawn;
+const spawnSync = require('child_process').spawnSync;
 
 program
     .parse(process.argv);
+
+// Run NPM to install the Yeoman generator
+var npm = spawnSync('npm', ['install', '-g', 'generator-swiftserver'], { stdio: 'inherit' });
+if (npm.error) {
+    console.error(chalk.red('Error: ') + 'NPM package installation failed');
+    process.exit(npm.status);
+}
 
 let child = spawn('yo', ['swiftserver', '--init'], { stdio: 'inherit' });
 child.on('error', (err) => {
