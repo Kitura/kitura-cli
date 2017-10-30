8fbe81826b3179384f4fd31d227528541465a3d0
Chris Bailey <baileyc@uk.ibm.com>
2017-10-30 12:43:55 +0000

    Update 'kit' option to downloaded latest release zip file (#1)
    * Update 'kit' option to downloaded latest release zip file
    
    * Improve error handling, add chalk require()
    
    * Add full stop

--- kitura-kit.js
+++ kitura-kit.js
@@ -1,5 +1,6 @@
 #!/usr/bin/env node
 
+const chalk = require('chalk');
 const fs = require('fs');
 const program = require('commander');
 const request = require('request');
@@ -7,18 +8,42 @@ const request = require('request');
 program
     .parse(process.argv);
 
-const url = 'https://api.github.com/repos/IBM-Swift/KituraKit/zipball';
+const options = {
+    method: "GET",
+    followAllRedirects: true,
+    headers: {
+        "user-agent": "node.js"
+    }
+};
+
+const url = 'https://api.github.com/repos/IBM-Swift/KituraKit/releases';
 const filename = 'KituraKit.zip';
 
 // Download KituraKit from GitHub
-console.log('Downloading...');
+console.log('Downloading KituraKit...');
 
-request
-    .get({url, followAllRedirects: true, headers: { 'User-Agent': 'node.js'}})
-    .on('error', (err) => {
-        console.error(chalk.red('Error: ') + 'download failed.');
-    })
-    .on('response', (response) => {
-        console.log('KituraKit downloaded to ' + filename);
-    })
-    .pipe(fs.createWriteStream(filename));
+request(url, options, function(error, response, body) {
+    if (error) {
+        console.error(chalk.red('Error: ') + 'failed to get releases from GitHub.');
+        return;
+    }
+
+    let latestKit;
+    try {
+        let releases = JSON.parse(body);
+        latestKit = releases[0].assets[0].browser_download_url
+    } catch (err) {
+        console.error(chalk.red('Error: ') + 'failed to find release URL from GitHub.');
+        return;
+    }
+
+    request
+        .get(latestKit, options)
+        .on('error', (err) => {
+            console.error(chalk.red('Error: ') + 'download failed.');
+        })
+        .on('response', (response) => {
+            console.log('KituraKit downloaded to ' + filename);
+        })
+        .pipe(fs.createWriteStream(filename));
+    });
