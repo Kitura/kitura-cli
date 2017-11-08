474816d8735dc365888a692bbf9d666ca1f28ce4
Ian Partridge <i.partridge@uk.ibm.com>
2017-11-08 17:58:51 +0000

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,5 +1,43 @@
 #!/bin/bash
 
-kitura --version
+if [ -z "$TMPDIR" ];then
+    TMPDIR=/tmp
+fi
+
+TESTDIR="$TMPDIR/kitura-cli-test"
+
+echo "Cleaning any prior test directory $TESTDIR"
+rm -rf "$TESTDIR"
+
+echo "Creating test directory $TESTDIR"
+mkdir -p "$TESTDIR"
+PATH=$TESTDIR/bin:$PATH
+
+echo "Installing kitura-cli"
+echo "- Creating package"
+PKG=$(npm pack) || exit 1
+
+echo "- Installing package ($PKG) to test directory as global module"
+if ! npm install -g --prefix="$TESTDIR" "$PKG"
+then
+    echo "Failed to install"
+    rm "$PKG"
+    rm -rf "$TESTDIR"
+    exit 1
+fi
+echo "Installation complete"
+rm "$PKG"
+
+cd "$TESTDIR" || exit 1
+
+if ! kitura --version
+then
+    echo "Failed"
+    rm -rf "$TESTDIR"
+    exit 1
+fi
+
+echo "Cleaning up test directory"
+rm -rf $TESTDIR
 
 echo "Testing Succeeded"
