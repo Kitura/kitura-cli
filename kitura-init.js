ec771a4ee8a49ec683557c73a3dbf827b3849556
Alex Wishart <awishart@uk.ibm.com>
2018-05-17 10:09:35 +0100

    Update kitura init to clone instead of running generator (#28)

--- kitura-init.js
+++ kitura-init.js
@@ -1,32 +1,145 @@
 #!/usr/bin/env node
 
 const chalk = require('chalk');
-const spawn = require('child_process').spawn;
+const spawnSync = require('child_process').spawnSync;
+const os = require('os');
+const fs = require('fs');
+const path = require('path');
+
+const initURL = 'https://github.com/IBM-Swift/generator-swiftserver-projects'
+const initBranch = 'init'
 
 let args = process.argv.slice(2);
-
-let options = ['-p', 'yo@1', '-p', 'generator-swiftserver', '--', 'yo', 'swiftserver'];
 
 if (args.length > 0) {
     if (args.indexOf('--help') > -1) {
         printHelp();
         process.exit(0);
     }
-
-    // Add on the arguments
-    options.push(args);
 }
 
-options.push('--init');
+let currentDirPath = path.resolve("./");
+let currentDir = path.basename(currentDirPath);
+// Replace spaces with hyphens so Xcode project will build.
+let projName = currentDir.replace(/ /g, "-");
 
-let child = spawn('npx', options, { stdio: 'inherit' });
-child.on('error', (err) => {
-    console.error(chalk.red('Error: ') + 'failed to run npx.');
-    console.error('Please check `node -v` is >= v8.2.0 and `npm -v` is >= 5.2.0.');
-});
-child.on('close', (code) => {
-    process.exit(code);
-});
+if (projName.charAt(0) === '.') {
+    console.error(chalk.red('Application name cannot start with .: %s'));
+    process.exit(1)
+}
+
+
+// Make sure directory is empty.
+checkCurrentDirIsEmpty();
+
+// Clone repo contents into current directory.
+cloneProject(initURL, initBranch);
+
+// Rename project to match current directory
+renameProject();
+
+// If '--skip-build' not specified, then build project.
+if (!(args.includes('--skip-build'))) {
+    buildProject();
+}
+
+function checkCurrentDirIsEmpty() {
+    try {
+        var data = fs.readdirSync('.');
+        if (data.length !== 0) {
+            console.error(chalk.red('Error:') + 'Current directory is not empty.');
+            console.error(chalk.red('Please repeat the command in an empty directory.'));
+            process.exit(1);
+        }
+    } catch (err) {
+        console.error(chalk.red('Error: ') + 'could not create project.');
+        console.error(err.message);
+        process.exit(err.errno);
+    }
+}
+
+function cloneProject(url, branch) {
+    console.log('Creating project...');
+    let clone = spawnSync('git', ['clone', '-b', branch, url, '.']);
+
+    if (clone.status !== 0) {
+        console.error(chalk.red('Error: ') + 'failed to run git clone.');
+        console.error('Please check your network connection and that you have git installed.');
+        console.error('Head to https://git-scm.com/downloads for instructions on installing git.');
+        process.exit(clone.status);
+    } else {
+        console.log(chalk.green('Project created successfully.'));
+        if ((args.includes('--skip-build'))) {
+            console.log('Next steps:');
+            console.log('');
+            console.log('Generate your Xcode project:');
+            console.log(chalk.grey('   $ swift package generate-xcodeproj'));
+            console.log('');
+            console.log('Or, build your project from the terminal:');
+            console.log(chalk.grey('   $ swift build'));
+        }
+    }
+
+    // Remove git remote
+    let git = spawnSync('rm', ['-rf', '.git']);
+    if (git.status !== 0) {
+        console.error(chalk.red('Error: ') + 'failed to remove .git directory.');
+        process.exit(git.status);
+    }
+}
+
+function renameProject() {
+    // Clean name can only contain alphanumeric characters
+    let projNameClean = projName.replace(/^[^a-zA-Z]*/, '')
+        .replace(/[^a-zA-Z0-9]/g, '');
+    let oldProjName = "generator-swiftserver-projects";
+    let oldProjNameClean = "generatorswiftserverprojects";
+
+    // Rename directories (charts can't contain special characters).
+    try {
+        fs.renameSync("./chart/" + oldProjNameClean, "./chart/" + projNameClean);
+        fs.renameSync("./Sources/" + oldProjName, "./Sources/" + projName);
+    } catch (err) {
+        console.error(chalk.red('Error: ') + 'could not rename directories.');
+        console.error(err.message);
+        process.exit(err.errno);
+    }
+    // Rename instances of project name (charts can't contain special characters)
+    var renameClean = spawnSync('find', ['.', '-exec', 'sed', '-i', '', 's/' + oldProjNameClean + '/' + projNameClean + '/g', '{}', ';'])
+    if (renameClean.status !== 0) {
+        console.error(chalk.red('Error: ') + 'could not rename project.');
+        console.error(renameClean.stderr.toString());
+        process.exit(renameClean.status);
+    }
+    var rename = spawnSync('find', ['.', '-exec', 'sed', '-i', '', 's/' + oldProjName + '/' + projName + '/g', '{}', ';'])
+    if (rename.status !== 0) {
+        console.error(chalk.red('Error: ') + 'could not rename project.');
+        console.error(rename.stderr.toString());
+        process.exit(rename.status);
+    }
+}
+
+function buildProject() {
+    console.log('Running `swift build` to build project...');
+
+    var build = spawnSync('swift', ['build'])
+    if (build.status !== 0) {
+        console.error(chalk.red('Failed to complete build.'));
+        console.log(build.stderr.toString());
+        process.exit(build.status);
+    } else {
+        console.log(chalk.green('Project built successfully.'));
+        console.log('Next steps:');
+        console.log('');
+        console.log('Generate your Xcode project:');
+        console.log(chalk.grey('   $ swift package generate-xcodeproj'));
+        console.log('');
+        console.log('Or, run your app from the terminal:');
+        console.log(chalk.grey('   $ .build/debug/' + projName));
+    }
+}
+
+
 
 function printHelp() {
     console.log("");
