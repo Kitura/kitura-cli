a20d08293ee183278de0771c2f917ff37eb76172
David Okun <david@okun.io>
2018-12-19 12:57:59 -0500

    Fixes #47

--- kitura-kit.js
+++ kitura-kit.js
@@ -8,5 +8,5 @@ program
 console.log('Add the following lines to your Podfile to install KituraKit into your iOS app using CocoaPods:');
 console.log('');
 console.log('# Pod for KituraKit');
-console.log('pod \'KituraKit\', :git => \'https://github.com/IBM-Swift/KituraKit.git\', :branch => \'pod\'');
+console.log('pod \'KituraKit\'');
 
