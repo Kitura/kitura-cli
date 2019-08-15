53a18581bfa01500dfe34e2d63cf78427147106b
David Jones <djones6@uk.ibm.com>
2019-08-15 15:22:16 +0100

    Fix packaging

--- build.sh
+++ build.sh
@@ -16,22 +16,25 @@ cp -R -p * $KITURA_PROJ/
 
 # Install dependencies
 # cobra (https://github.com/spf13/cobra)
-go get -u github.com/spf13/cobra/cobra
+go get -v -u github.com/spf13/cobra/cobra
 # go-git (https://github.com/src-d/go-git)
-go get -u gopkg.in/src-d/go-git.v4/...
+go get -v -u gopkg.in/src-d/go-git.v4/...
+
+# Set umask to ensure deb contents have right permissions
+umask 022
+chmod -R 755 linux-amd64
 
 # Build for Mac
 cd $KITURA_PROJ
 mkdir -p $PROJ_SRC/darwin-amd64
-env GOOS=darwin GOARCH=amd64 go build -o $PROJ_SRC/darwin-amd64/kitura
+env GOOS=darwin GOARCH=amd64 go build -v -o $PROJ_SRC/darwin-amd64/kitura
 
 # Build for Linux
 mkdir -p $PROJ_SRC/linux-amd64/usr/local/bin
-env GOOS=linux GOARCH=amd64 go build -o $PROJ_SRC/linux-amd64/usr/local/bin/kitura
+env GOOS=linux GOARCH=amd64 go build -v -o $PROJ_SRC/linux-amd64/usr/local/bin/kitura
 
 # Package macOS binary into downloadable tar.gz
-cd $PROJ_SRC && tar -czf kitura-cli-${RELEASE}.tar.gz darwin-amd64
+cd $PROJ_SRC && tar -czf kitura-cli-${RELEASE}_darwin.tar.gz darwin-amd64
 
 # Package linux binary into .deb
-cd $PROJ_SRC && ln -s linux-amd64 kitura-cli_${RELEASE} && dpkg-deb --build kitura-cli_${RELEASE} && rm kitura-cli_${RELEASE}
-
+cd $PROJ_SRC && cp -R -p linux-amd64 kitura-cli_${RELEASE} && dpkg-deb --build kitura-cli_${RELEASE} && mv kitura-cli_${RELEASE}.deb kitura-cli_${RELEASE}_amd64.deb && rm -r kitura-cli_${RELEASE}
