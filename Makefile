9bb42d5d75aaa643b1708f3c9fe1404cd79b7402
David Jones <djones6@uk.ibm.com>
2019-08-16 14:08:38 +0100

    Copy files before writing placeholders

---
+++ Makefile
@@ -0,0 +1,52 @@
+GOCMD=go
+GOBUILD=$(GOCMD) build
+GOGET=$(GOCMD) get
+GOCLEAN=$(GOCMD) clean
+
+BINARY_NAME=kitura
+PACKAGE_NAME=kitura-cli
+LINUX_DIR=linux-amd64
+LINUX_PATH=/usr/local/bin
+LINUX_BINARY=$(LINUX_DIR)$(LINUX_PATH)/$(BINARY_NAME)
+MACOS_DIR=darwin-amd64
+MACOS_PATH=/
+MACOS_BINARY=$(MACOS_DIR)$(MACOS_PATH)/$(BINARY_NAME)
+
+GOPATH=$(HOME)/kitura-cli-$(RELEASE)
+KITURA_SRC=$(GOPATH)/src/kitura
+
+all: build package
+build: setup deps build-linux build-darwin
+package: package-linux package-darwin
+clean: 
+	$(GOCLEAN)
+	rm -f $(LINUX_BINARY)
+	rm -f $(MACOS_BINARY)
+setup:
+	# Copy kitura/cmd module into GOPATH
+	mkdir -p $(KITURA_SRC)
+	cp -R -p cmd $(KITURA_SRC)
+	# Replace release placeholders in sources
+	cp install.sh.ver install.sh
+	cp $(LINUX_DIR)/DEBIAN/control.ver $(LINUX_DIR)/DEBIAN/control
+	sed -i -e"s#@@RELEASE@@#$(RELEASE)#g" install.sh $(LINUX_DIR)/DEBIAN/control $(KITURA_SRC)/cmd/root.go
+deps:
+	# Install dependencies
+	$(GOGET) github.com/spf13/cobra/cobra
+	$(GOGET) gopkg.in/src-d/go-git.v4/...
+
+build-linux: setup deps
+	GOOS=linux GOARCH=amd64 $(GOBUILD) -o $(LINUX_BINARY) -v
+
+build-darwin: setup deps
+	GOOS=darwin GOARCH=amd64 $(GOBUILD) -o $(MACOS_BINARY) -v
+
+package-linux: build-linux
+	cp -R -p $(LINUX_DIR) $(PACKAGE_NAME)_$(RELEASE)
+	chmod -R 755 $(LINUX_DIR)
+	dpkg-deb --build $(PACKAGE_NAME)_$(RELEASE)
+	mv $(PACKAGE_NAME)_$(RELEASE).deb $(PACKAGE_NAME)_$(RELEASE)_amd64.deb
+	rm -r $(PACKAGE_NAME)_$(RELEASE)
+
+package-darwin: build-darwin
+	tar -czf $(PACKAGE_NAME)-$(RELEASE)_darwin.tar.gz $(MACOS_DIR)
--- build.sh
+++ build.sh
@@ -7,48 +7,7 @@ if [ -z "$1" ]; then
     exit 1
 fi
 
-export PROJ_SRC=$PWD
-
 export RELEASE=$1
-
-export GOPATH=$HOME/kitura-cli-$RELEASE
-KITURA_PROJ=$GOPATH/src/kitura
-
-# Write version number into sources
-sed -i -e"s#@@RELEASE@@#${RELEASE}#g" install.sh linux-amd64/DEBIAN/control cmd/root.go
-
-# Copy sources into right location for Go to find them
-mkdir -p $KITURA_PROJ
-cp -R -p * $KITURA_PROJ/
-
-# Install dependencies
-# cobra (https://github.com/spf13/cobra)
-go get -v github.com/spf13/cobra/cobra
-# go-git (https://github.com/src-d/go-git)
-go get -v gopkg.in/src-d/go-git.v4/...
-
-# Set umask to ensure deb contents have right permissions
-umask 022
-chmod -R 755 linux-amd64
-
-function build_Darwin() {
-    # Build for Mac
-    cd $KITURA_PROJ
-    mkdir -p $PROJ_SRC/darwin-amd64
-    env GOOS=darwin GOARCH=amd64 go build -v -o $PROJ_SRC/darwin-amd64/kitura
-
-    # Package macOS binary into downloadable tar.gz
-    cd $PROJ_SRC && tar -czf kitura-cli-${RELEASE}_darwin.tar.gz darwin-amd64
-}
-
-function build_Linux() {
-    # Build for Linux
-    mkdir -p $PROJ_SRC/linux-amd64/usr/local/bin
-    env GOOS=linux GOARCH=amd64 go build -v -o $PROJ_SRC/linux-amd64/usr/local/bin/kitura
-
-    # Package linux binary into .deb
-    cd $PROJ_SRC && cp -R -p linux-amd64 kitura-cli_${RELEASE} && dpkg-deb --build kitura-cli_${RELEASE} && mv kitura-cli_${RELEASE}.deb kitura-cli_${RELEASE}_amd64.deb && rm -r kitura-cli_${RELEASE}
-}
 
 function failCmdFound() {
     echo "Error - 'kitura' command already exists"
@@ -92,11 +51,11 @@ function test_Linux() {
 
 case `uname` in
   Darwin)
-    build_Darwin
+    make build-darwin package-darwin
     test_Darwin
     ;;
   Linux)
-    build_Linux
+    make
     test_Linux
     ;;
   *)
